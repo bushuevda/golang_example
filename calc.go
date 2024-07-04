88f03d6bb838923525fd6a39ccc4b04c5e196b13
Dmitrii <73591672+bushuevda@users.noreply.github.com>
2024-07-04 18:33:48 +0300

    Update calc.go

--- calc.go
+++ calc.go
@@ -1,9 +1,10 @@
 package calc
-
+//Сложение Adding
 func Add(first float64, second float64) float64 {
 	return first + second
 }
 
+//Вычитание
 func Sub(first float64, second float64) float64 {
 	return first - second
 }
